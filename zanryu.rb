ce9536d034e4663b5a009fece1b473e0a874fd71
Hiromu Kamei <hk.radwimps@gmail.com>
2018-11-09 01:13:29 +0900

    Added error handling

--- zanryu.rb
+++ zanryu.rb
@@ -18,7 +18,7 @@ cns_password = config['cns_password']
 my_phone_num = config['my_phone_num']
 emergency_phone_num = config['emergency_phone_num']
 estimated_time_to_stay = config['estimated_time_to_stay']
-building = config['building'] # Z => 8
+building = config['building']
 floor = config['floor']
 room = config['room']
 reason = config['reason']
@@ -29,9 +29,16 @@ sfs_res = Net::HTTP.post_form(sfs_url, {
   u_pass: cns_password,
   lang: 'ja'
 })
+login_result = sfs_res.body.match(/\A.+id=(.+)&type=.+\Z/)
+if sfs_res.code != '200'
+  raise 'SFC-SFS login.cgi URL is incorrect'
+elsif login_result.nil?
+  raise 'Could not login SFC-SFS'
+else
+  enc_id = login_result[1]
+end
 
 stay_url = URI.parse(stay_url)
-enc_id = sfs_res.body.match(/\A.+id=(.+)&type=.+\Z/)[1]
 stay_url.query = URI.encode_www_form({
   yc: yc,
   ks: ks,
@@ -39,7 +46,7 @@ stay_url.query = URI.encode_www_form({
 })
 Net::HTTP.get_response(stay_url)
 
-Net::HTTP.post_form(stay_url, {
+stay_res = Net::HTTP.post_form(stay_url, {
   stay_phone: my_phone_num,
   stay_p_phone: emergency_phone_num,
   stay_time: estimated_time_to_stay,
@@ -52,5 +59,10 @@ Net::HTTP.post_form(stay_url, {
   mode: 'submit',
   enc_id: enc_id
 })
-
-puts 'Successfully submited!'
+if stay_res.code != '200'
+  raise 'SFC-SFS stay_input.cgi URL is incorrect'
+elsif !stay_res.body.include?('form name="cancel"')
+  raise 'Insufficient submitted information '
+else
+  puts 'Successfully submited!'
+end
